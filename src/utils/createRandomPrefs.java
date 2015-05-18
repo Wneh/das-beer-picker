df59846b2a38a50fee829716a18388172cd7f616
Carl Eriksson <carle293@hotmail.com>
2015-05-18 14:18:03 +0200

    Add code for creating users and inserting into database

--- src/utils/createRandomPrefs.java
+++ src/utils/createRandomPrefs.java
@@ -1,12 +1,15 @@
 package utils;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class createRandomPrefs {
+	
+	public static final int COUNTER = 1000;
+	public static final int PERSON_COUNTER = 1000;
 
 	public static void main(String args[]){
 		
@@ -14,8 +17,13 @@ public class createRandomPrefs {
 		
 		Connection connection = c.getConnection();
 		
-		createPrefs(3,3);
+		ArrayList<ArrayList<Double>> data = createPrefs(COUNTER,3);
 		
+		createTable(connection);
+		
+		insertPrefs(connection,data);
+		
+		createPersons(connection);
 	}
 	
 	/**
@@ -28,8 +36,7 @@ public class createRandomPrefs {
 	 */
 	public static ArrayList<ArrayList<Double>> createPrefs(int n, int k){
 		ArrayList<ArrayList<Double>> rows = new ArrayList<ArrayList<Double>>(n);
-		
-		
+			
 		for(int j = 0; j < n; j++){
 			ArrayList<Double> row = new ArrayList<Double>(k);
 			int rowSum = 0;
@@ -41,8 +48,7 @@ public class createRandomPrefs {
 				row.add(temp);
 				max -= newNumber;
 			}
-			System.out.println("max: " + max);
-			System.out.println("min: " + min);
+			
 			row.add((double)(max-min)/100);
 			
 			//check so the sum is 1
@@ -57,10 +63,81 @@ public class createRandomPrefs {
 			System.out.println(row);
 			rows.add(row);
 		}
-		
-		
-		
 		return rows;
+	}
+	
+	private static void insertPrefs(Connection connection, ArrayList<ArrayList<Double>> data){
+		String insertString = "INSERT INTO prefs VALUES(?,?,?,?)";
+		
+		PreparedStatement insertPrefs = null;
+		try {
+			insertPrefs = connection.prepareStatement(insertString);
+			int counter = 0;
+			for(ArrayList<Double> row : data){
+				insertPrefs.setInt(1,counter);
+				insertPrefs.setDouble(2,row.get(0));
+				insertPrefs.setDouble(3,row.get(1));
+				insertPrefs.setDouble(4,row.get(2));
+				insertPrefs.executeUpdate();
+//				connection.commit();
+				counter++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creates a database table for inserting the preference data
+	 * 
+	 * @param connection
+	 */
+	private static void createTable(Connection connection){
+		PreparedStatement ps;
+		try {
+			ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS prefs(prefs_if SERIAL NOT NULL PRIMARY KEY,pref1 double precision, pref2 double precision, pref3 double precision)");
+			ps.executeUpdate();
+			ps.close();
+			System.out.println("Created prefs table");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void createPersons(Connection connection){
+		createTablePersons(connection);
+		String insertString = "INSERT INTO persons VALUES(?,?,?,?)";
+
+		PreparedStatement insertPrefs = null;
+		try {
+			insertPrefs = connection.prepareStatement(insertString);
+			for(int i = 0; i < PERSON_COUNTER; i++){
+				insertPrefs.setInt(1,i);
+				insertPrefs.setInt(2,randomNumber(18,65));
+				insertPrefs.setInt(3,randomNumber(140,200));
+				insertPrefs.setInt(4,randomNumber(50,120));
+				insertPrefs.executeUpdate();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void createTablePersons(Connection connection){
+		PreparedStatement ps;
+		try {
+			ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS persons(persons_id SERIAL NOT NULL PRIMARY KEY,age int, length int, weight int)");
+			ps.executeUpdate();
+			ps.close();
+			System.out.println("Created prefs table");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	private static int randomNumber(int min, int max){
