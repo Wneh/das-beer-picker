c876d09265b8981c73f210303cf8f7ab41836ad3
stringdahl <erik.ringdahl@gmail.com>
2015-05-18 15:08:10 +0200

    added ideal constants

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 public class Main {
+	private final int IDEAL_AGE = 25;
+	private final int IDEAL_WEIGHT = 65;
+	private final int IDEAL_LENGTH = 175;
+
 
 	public Main(){
 		Connection connection = new utils.DatabaseConnection().getConnection();
