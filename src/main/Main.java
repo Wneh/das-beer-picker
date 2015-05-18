5f25360f65b92dd64f3f85e20e09338680b8a464
stringdahl <erik.ringdahl@gmail.com>
2015-05-18 15:50:05 +0200

    added centroid function and weighted values

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,37 @@ public class Main {
 //		ArrayList<>
 		return null;
 	}
-	
+
+	public ArrayList<Integer> calculateCentroid(ArrayList<Person> persons){
+		ArrayList<Integer> centroid = new ArrayList<Integer>();
+		int a = 0, l = 0, w = 0;
+		ArrayList<Integer> tmpWeights;
+		for (Person person : persons){
+			tmpWeights = calculateWeightedValues(person);
+			a += tmpWeights.get(0);
+			l += tmpWeights.get(1);
+			w += tmpWeights.get(2);
+		}
+		centroid.add(a/persons.size());
+		centroid.add(l/persons.size());
+		centroid.add(w/persons.size());
+		return centroid;
+	}
+
+	public ArrayList<Integer> calculateWeightedValues(Person person){
+		ArrayList<Integer> weights = new ArrayList<Integer>();
+
+		int a = 10 - (Math.abs(IDEAL_AGE - person.age) / 5);
+		int l = 10 - (Math.abs(IDEAL_LENGTH - person.length) / 6);
+		int w = 10 - (Math.abs(IDEAL_WEIGHT - person.weight) / 7);
+
+		weights.add(a);
+		weights.add(l);
+		weights.add(w);
+		return weights;
+	}
+
+
 	public static void main(String[] args) {
 		new Main();
 	}
