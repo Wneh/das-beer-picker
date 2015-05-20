319ccb7d497b5481f5105b545ac86c6e06356d05
Carl Eriksson <carle293@hotmail.com>
2015-05-20 13:57:57 +0200

    Add comments for Person class

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,5 +1,12 @@
 package main;
 
+/**
+ * Person class that represent the data stored in the
+ * persons table in the database.
+ * 
+ * Also implements comparable based on the cosine similarity
+ * The cosine score is calculated with the CosineSimilarity class
+ */
 public class Person implements Comparable<Person> {
 
 	public int id;
@@ -20,6 +27,14 @@ public class Person implements Comparable<Person> {
 		return "id: " + this.id + ", age: " + this.age + ", length: " + this.length + ", weight: " + this.weight +", cosineScore: "+ this.cosineScore + "\n";
 	}
 
+	/**
+	 * Creates and double[] array with the following scheme:
+	 * 		Index 0: age
+	 * 		Index 1: length
+	 * 		Index 2: weight
+	 * 
+	 * @return double[] array with age, length and weight
+	 */
 	public double[] toArray(){
 		double [] arr = new double[3];
 		arr[0] = age;
@@ -28,6 +43,9 @@ public class Person implements Comparable<Person> {
 		return arr;
 	}
 
+	/**
+	 * Compares two Person object based on the cosine score.
+	 */
 	public int compareTo(Person other){
 		if (this.cosineScore < other.cosineScore) return -1;
 		else if (this.cosineScore > other.cosineScore) return 1;
